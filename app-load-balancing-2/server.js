ed3223b155d33b62a6445ccce6bfbf7e44169014
balinux <ryojuniyantara@gmail.com>
2022-08-13 10:43:41 +0800

    initial commit

---
+++ app-load-balancing-2/server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+/** @type {typeof import('telegraf').Telegraf} */
+const {Telegraf} = require('telegraf')
+
+// Constants
+const PORT = 8082;
+const HOST = '0.0.0.0';
+
+// // telegraf
+// const bot = new Telegraf(process.env.Bot_Token)
+
+// bot.on('text', (ctx) => {
+
+//   // Explicit usage
+//   // ctx.telegram.sendMessage(ctx.message.chat.id, `Hello anda mengirimkan pesan ${ctx.message.text} ke saya`)
+
+//   // Using context shortcut
+//   ctx.reply(`Hello anda mengirimkan pesan ${ctx.message.text} ke saya`)
+// })
+
+// bot.launch()
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World app 2');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
